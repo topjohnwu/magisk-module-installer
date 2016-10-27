1af815bbf6acd87aaaab08a50a86885974566709
blake1029384756 <blakemammen@centurytel.net>
2016-10-27 18:18:34 -0500

    initial commit

--- config.sh
+++ config.sh
@@ -36,7 +36,7 @@ CACHEMOD=false
 
 # This will be the folder name under /magisk or /cache/magisk
 # This should also be the same as the id in your module.prop to prevent confusion
-MODID=template
+MODID=IOS-10-Emoji
 
 # Set to true if you need automount
 # Most mods would like it to be enabled
@@ -59,7 +59,7 @@ LATESTARTSERVICE=false
 
 print_modname() {
   ui_print "*******************************"
-  ui_print "     Magisk Module Template    "
+  ui_print "           IOS 10 Emoji        "
   ui_print "*******************************"
 }
 
@@ -70,14 +70,6 @@ print_modname() {
 # List all directories you want to directly replace in the system
 # By default Magisk will merge your files with the original system
 # Directories listed here however, will be directly mounted to the correspond directory in the system
-
-# This is an example
-REPLACE="
-/system/app/Youtube
-/system/priv-app/SystemUI
-/system/priv-app/Settings
-/system/framework
-"
 
 # Construct your own list
 REPLACE="
