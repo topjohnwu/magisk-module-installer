5dbdc878080d47551708977fe47aa8be74a91a67
topjohnwu <topjohnwu@gmail.com>
2016-11-15 02:51:13 +0800

    Remove cache modules

--- common/post-fs.sh
+++
@@ -1,4 +0,0 @@
-#!/system/bin/sh
-
-# This script will be executed in post-fs mode
-# More info in the main Magisk thread
--- config.sh
+++ config.sh
@@ -31,9 +31,6 @@
 
 # NOTE: This part has to be adjusted to fit your own needs
 
-# Is this a cache mod?
-CACHEMOD=false
-
 # This will be the folder name under /magisk or /cache/magisk
 # This should also be the same as the id in your module.prop to prevent confusion
 MODID=template
@@ -42,13 +39,10 @@ MODID=template
 # Most mods would like it to be enabled
 AUTOMOUNT=true
 
-# Set to true if you need post-fs script (Only available in cache mods)
-POSTFS=false
-
-# Set to true if you need post-fs-data script (Only available in non-cache mods)
+# Set to true if you need post-fs-data script
 POSTFSDATA=false
 
-# Set to true if you need late_start service script (Only available in non-cache mods)
+# Set to true if you need late_start service script
 LATESTARTSERVICE=false
 
 ##########################################################################################
@@ -79,7 +73,7 @@ REPLACE="
 /system/framework
 "
 
-# Construct your own list
+# Construct your own list here
 REPLACE="
 "
 
