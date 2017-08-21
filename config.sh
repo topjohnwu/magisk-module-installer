0952c737b42974de52ccd51f8d0ec17ada6f2004
Atarii <atarii@users.noreply.github.com>
2017-08-21 22:29:07 +0100

    Update config.sh
    removed set_perm on HwCallRecorder

--- config.sh
+++ config.sh
@@ -102,5 +102,5 @@ set_permissions() {
   # set_perm  $MODPATH/system/bin/app_process32   0       2000    0755         u:object_r:zygote_exec:s0
   # set_perm  $MODPATH/system/bin/dex2oat         0       2000    0755         u:object_r:dex2oat_exec:s0
   # set_perm  $MODPATH/system/lib/libart.so       0       0       0644
-  set_perm  $MODPATH/system/app/HwCallRecorder/HwCallRecorder.apk       0       0       0644
+  #set_perm  $MODPATH/system/app/HwCallRecorder/HwCallRecorder.apk       0       0       0644
 }
