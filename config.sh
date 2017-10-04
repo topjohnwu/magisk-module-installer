65124f748f2229852883a479d9899762fc6bc039
Atarii <atarii@users.noreply.github.com>
2017-10-04 20:22:47 +0100

    Updated base template to v1400

--- config.sh
+++ config.sh
@@ -18,14 +18,8 @@
 ##########################################################################################
 
 ##########################################################################################
-# Defines
+# Configs
 ##########################################################################################
-
-# NOTE: This part has to be adjusted to fit your own needs
-
-# This will be the folder name under /magisk
-# This should also be the same as the id in your module.prop to prevent confusion
-MODID=HuaweiCallRecorder
 
 # Set to true if you need to enable Magic Mount
 # Most mods would like it to be enabled
@@ -84,16 +78,11 @@ REPLACE="
 # Permissions
 ##########################################################################################
 
-# NOTE: This part has to be adjusted to fit your own needs
-
 set_permissions() {
-  # Default permissions, don't remove them
-  set_perm_recursive  $MODPATH  0  0  0755  0644
-
   # Only some special files require specific permissions
   # The default permissions should be good enough for most cases
 
-  # Some templates if you have no idea what to do:
+  # Here are some examples for the set_perm functions:
 
   # set_perm_recursive  <dirname>                <owner> <group> <dirpermission> <filepermission> <contexts> (default: u:object_r:system_file:s0)
   # set_perm_recursive  $MODPATH/system/lib       0       0       0755            0644
@@ -102,5 +91,7 @@ set_permissions() {
   # set_perm  $MODPATH/system/bin/app_process32   0       2000    0755         u:object_r:zygote_exec:s0
   # set_perm  $MODPATH/system/bin/dex2oat         0       2000    0755         u:object_r:dex2oat_exec:s0
   # set_perm  $MODPATH/system/lib/libart.so       0       0       0644
-  #set_perm  $MODPATH/system/app/HwCallRecorder/HwCallRecorder.apk       0       0       0644
+
+  # The following is default permissions, DO NOT remove
+  set_perm_recursive  $MODPATH  0  0  0755  0644
 }
