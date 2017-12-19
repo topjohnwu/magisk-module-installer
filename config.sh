ac53b8be8c86ecbff2ce1ced53e1405f4b1f346a
Tomas Sandelin <tomas.sandelin@gmail.com>
2017-12-19 21:23:37 +0200

    Did everything
    Added all the files and changed module.prop

--- config.sh
+++ config.sh
@@ -42,7 +42,7 @@ LATESTARTSERVICE=false
 
 print_modname() {
   ui_print "*******************************"
-  ui_print "     Magisk Module Template    "
+  ui_print "        Razerify Sounds        "
   ui_print "*******************************"
 }
 
