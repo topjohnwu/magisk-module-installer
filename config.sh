b54fb725f05562a30414414b353419dca298d8e8
Tomas Sandelin <tomas.sandelin@gmail.com>
2017-12-19 22:24:35 +0200

    removed replace line

--- config.sh
+++ config.sh
@@ -66,7 +66,6 @@ REPLACE="
 # Construct your own list here, it will overwrite the example
 # !DO NOT! remove this if you don't need to replace anything, leave it empty as it is now
 REPLACE="
-/system/media/audio/ui
 "
 
 ##########################################################################################
