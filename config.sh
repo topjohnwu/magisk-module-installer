81466d6726fee78343ab8baf0b93e73959adce44
Artoriarcher <38772301+Artoriarcher@users.noreply.github.com>
2018-04-27 10:10:52 +0800

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,37 +1,19 @@
 ##########################################################################################
-#
-# Magisk Module Template Config Script
-# by topjohnwu
-#
-##########################################################################################
-##########################################################################################
-#
-# Instructions:
-#
-# 1. Place your files into system folder (delete the placeholder file)
-# 2. Fill in your module's info into module.prop
-# 3. Configure the settings in this file (config.sh)
-# 4. If you need boot scripts, add them into common/post-fs-data.sh or common/service.sh
-# 5. Add your additional or modified system properties into common/system.prop
-#
-##########################################################################################
-
-##########################################################################################
 # Configs
 ##########################################################################################
 
 # Set to true if you need to enable Magic Mount
 # Most mods would like it to be enabled
-AUTOMOUNT=true
+AUTOMOUNT=false
 
 # Set to true if you need to load system.prop
-PROPFILE=false
+PROPFILE=true
 
 # Set to true if you need post-fs-data script
 POSTFSDATA=false
 
 # Set to true if you need late_start service script
-LATESTARTSERVICE=false
+LATESTARTSERVICE=true
 
 ##########################################################################################
 # Installation Message
@@ -40,9 +22,10 @@ LATESTARTSERVICE=false
 # Set what you want to show when installing your mod
 
 print_modname() {
-  ui_print "*******************************"
-  ui_print "     Magisk Module Template    "
-  ui_print "*******************************"
+  ui_print "************************************"
+  ui_print "  Force Disable Bionic ld Warning   "
+  ui_print "            :)  Artoriarcher        "               "
+  ui_print "************************************"
 }
 
 ##########################################################################################
@@ -51,14 +34,6 @@ print_modname() {
 
 # List all directories you want to directly replace in the system
 # Check the documentations for more info about how Magic Mount works, and why you need this
-
-# This is an example
-REPLACE="
-/system/app/Youtube
-/system/priv-app/SystemUI
-/system/priv-app/Settings
-/system/framework
-"
 
 # Construct your own list here, it will override the example above
 # !DO NOT! remove this if you don't need to replace anything, leave it empty as it is now
