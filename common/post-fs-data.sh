3f0943042a8f32fe028d839d73fc54d6967188a7
Ingvar <46319573+iDevil777@users.noreply.github.com>
2019-01-03 21:13:47 +0500

    Update post-fs-data.sh

--- common/post-fs-data.sh
+++ common/post-fs-data.sh
@@ -5,3 +5,13 @@ MODDIR=${0%/*}
 
 # This script will be executed in post-fs-data mode
 # More info in the main Magisk thread
+sed 's/ORANGE/GREEN/i' /proc/cmdline | sed 's/YELLOW/GREEN/i' > /data/local/tmp/cmdline
+mount -o bind /data/local/tmp/cmdline /proc/cmdline
+
+sed 's;^ro.*\.build\.fingerprint=.*;ro.build.fingerprint=Xiaomi/nitrogen/nitrogen:8.1.0/OPM1.171019.019/V10.2.1.0.OEDMIFK:user/release-keys;' /system/build.prop > /data/local/tmp/build.prop
+mount -o bind /data/local/tmp/build.prop /system/build.prop
+# The build.prop thing seems to be useless, but at some point Google are sure to catch on and check in the raw build.prop to see if we're abusing getprop/resetprop.
+resetprop ro.build.fingerprint 'Xiaomi/nitrogen/nitrogen:8.1.0/OPM1.171019.019/V10.2.1.0.OEDMIFK:user/release-keys'
+resetprop ro.bootimage.build.fingerprint 'Xiaomi/nitrogen/nitrogen:8.1.0/OPM1.171019.019/V10.2.1.0.OEDMIFK:user/release-keys'
+#resetprop ro.vendor.build.fingerprint 'Xiaomi/nitrogen/nitrogen:8.1.0/OPM1.171019.019/V10.2.1.0.OEDMIFK:user/release-keys'
+#The above caused issues (critical services not starting) on my Mi Max 3
