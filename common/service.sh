69dea0ff77fa564b1d150ef5ed6ac20571df23a4
crok <crok.bic@gmail.com>
2019-03-28 21:07:37 +0100

    Update service.sh
    # migration to new template

--- common/service.sh
+++ common/service.sh
@@ -1,28 +1,27 @@
 #!/system/bin/sh
-# Please don't hardcode /magisk/modname/... ; instead, please use $MODDIR/...
-# This will make your scripts compatible even if Magisk change its mount point in the future
+# Do NOT assume where your module will be located.
+# ALWAYS use $MODDIR if you need to know where this script
+# and module is placed.
+# This will make sure your module will still work
+# if Magisk change its mount point in the future
 MODDIR=${0%/*}
 
 # This script will be executed in late_start service mode
-# More info in the main Magisk thread
 
 # Wait..
 sleep 30
-$MODDIR/./tweak
-./tweak
 # Virtual memory tweaks
 stop perfd
-echo '30' > /proc/sys/vm/swappiness
 echo '0' > /sys/module/lowmemorykiller/parameters/enable_adaptive_lmk
 echo '80' > /proc/sys/vm/overcommit_ratio
 echo '400' > /proc/sys/vm/vfs_cache_pressure
 echo '24300' > /proc/sys/vm/extra_free_kbytes
 echo '128' > /proc/sys/kernel/random/read_wakeup_threshold
 echo '256' > /proc/sys/kernel/random/write_wakeup_threshold
-echo '1024' > /sys/block/mmcblk0/queue/read_ahead_kb
+echo '128' > /sys/block/mmcblk0/queue/read_ahead_kb
 echo '0' > /sys/block/mmcblk0/queue/iostats
 echo '1' > /sys/block/mmcblk0/queue/add_random
-echo '1024' > /sys/block/mmcblk1/queue/read_ahead_kb
+echo '128' > /sys/block/mmcblk1/queue/read_ahead_kb
 echo '0' > /sys/block/mmcblk1/queue/iostats
 echo '1' > /sys/block/mmcblk1/queue/add_random
 echo '4096' > /proc/sys/vm/min_free_kbytes
@@ -32,3 +31,5 @@ echo '70' > /proc/sys/vm/dirty_background_ratio
 chmod 666 /sys/module/lowmemorykiller/parameters/minfree
 chown root /sys/module/lowmemorykiller/parameters/minfree
 echo '21816,29088,36360,43632,50904,65448' > /sys/module/lowmemorykiller/parameters/minfree
+rm /data/system/perfd/default_values
+start perfd
